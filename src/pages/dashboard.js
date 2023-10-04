5003c4a7ff7b652ac130170b6a4194d6ed3541e6
yashkanjariyaa <yashkanjariyaff@gmail.com>
2023-10-04 10:09:58 +0530

    initial commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,19 +1,41 @@
 import React from "react";
-import AppBar from "../components/appBar"
-import jwt from "jsonwebtoken";
+import AppBar from "../components/appBar";
 import { useNavigate } from "react-router-dom";
 
-export default function Dashboard(){
-    const token = localStorage.getItem('token');
-    const navigate = useNavigate();
-    if(token){
-    const user = jwt.decode(token);
-        if(!user){
-            localStorage.removeItem('token');
-            navigate('/login', { replace : true });
+export default function Dashboard() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  async function check() {
+    try {
+      const response = await fetch("http://localhost:1337/api/check", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token,
+        }),
+      });
+      console.log(response);
+      const data = await response.json();
+      const validity = data.status;
+      if (!data.error) {
+        console.log(validity);
+        if (validity === "invalid") {
+          localStorage.removeItem("token");
+          navigate('/sign-in');
+        } else if (validity === "valid") {
+          console.log("user authenticated!");
+        } else {
+          console.log("error during authentification");
         }
+      } else {
+        console.log(data.error);
+      }
+    } catch (err) {
+      console.log(err);
     }
-    return(
-        <AppBar/>
-    );
+  }
+  check();
+  return <AppBar />;
 }
