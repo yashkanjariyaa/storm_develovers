54fbbd961a377d27bc443f5cced3330e986080cd
yashkanjariyaa <yashkanjariyaff@gmail.com>
2023-10-04 21:07:54 +0530

    small commit

--- models/authSchema.js
+++ models/authSchema.js
@@ -5,6 +5,7 @@ const User = new Schema(
         name : { type : String, required : true, unique: true},
         email : { type : String, unique : true },
         password : { type : String, required : true },
+        role : { type : String, required : true}
     });
 const model = mongoose.model('UserAuth',User);
 module.exports = model;
